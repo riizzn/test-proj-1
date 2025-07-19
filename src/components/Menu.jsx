855ae0d36b91e57a0c0ecaa02d260618a5c8afa7
rana <ranabdulsalam@gmail.com>
2025-07-19 16:52:21 +0530

    completed menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,10 +2,30 @@
 import React, { useRef, useState } from "react";
 
 import { allCocktails } from "../../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Menu = () => {
+   
+ 
+
   const contentref = useRef();
   const [currentIndex, setCurrentIndex] = useState(0);
+    useGSAP(() => {
+    gsap.fromTo("#title", { opacity: 0 }, { opacity: 1, duration: 1 });
+    gsap.fromTo(
+      ".cocktail img",
+      { opacity: 0, xPercent: -100 },
+      {
+        xPercent: 0,
+        opacity: 1,
+        duration: 0.5,
+        ease: "power1.inOut",
+      }
+    );
+    gsap.fromTo('.details h2', {yPercent:100, opacity:0}, {yPercent : 0 , opacity :100,ease:'power1.inOut'})
+    gsap.fromTo('.details p', {yPercent:100, opacity:0}, {yPercent : 0 , opacity :100,ease:'power1.inOut'})
+  }, [currentIndex]);
   const len = allCocktails.length;
   const goToSlide = (index) => {
     const newIndex = (index + len) % len;
